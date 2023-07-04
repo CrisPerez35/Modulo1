9903dbbf2489488793bc897f12d3a19d2bbc058c
CrisPerez35 <138174687+CrisPerez35@users.noreply.github.com>
2023-07-03 22:28:59 -0400

    vSe agrega el proyecto

---
+++ Modulo1.js
@@ -0,0 +1,5 @@
+var saludo=alert("Bienvenido a la página! ");
+var saludo_final="Que tengas una buena experiencia en nuestra página "
+var nombre=prompt("Ingrese su nombre",);
+saludo_final=saludo_final.concat(nombre);
+alert(saludo_final)
